883d0a7f467383993660b1494da36fc3492d74b2
Mesut <mesutahmettravaci@gmail.com>
2024-04-28 00:36:01 +0300

    Add files via upload

---
+++ training/shutilmove.py
@@ -0,0 +1,5 @@
+import shutil
+
+shutil.move('tennis-ball-detection-6/train','tennis-ball-detection-6/tennis-ball-detection-6/train')
+shutil.move('tennis-ball-detection-6/test','tennis-ball-detection-6/tennis-ball-detection-6/test')
+shutil.move('tennis-ball-detection-6/valid','tennis-ball-detection-6/tennis-ball-detection-6/valid')
